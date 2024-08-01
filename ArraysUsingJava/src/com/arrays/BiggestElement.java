c7a2be8d40365732032b2726c6fcb65fae96e4be
Aniket212003 <aniketbelgaonakar21@gmail.com>
2024-08-01 21:47:59 +0530

    Biggest Element Form Array Program

---
+++ ArraysUsingJava/src/com/arrays/BiggestElement.java
@@ -0,0 +1,23 @@
+package com.arrays;
+
+public class BiggestElement 
+{
+	public static Integer greatestElement(Integer arr[])
+	{
+		int max = 0;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i] > max)
+			{
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+	public static void main(String[] args) 
+	{
+		Integer array[] = {2,6,1,9,1,4};
+		Integer num = greatestElement(array);
+		System.out.println("Biggest Element : " + num);
+	}
+}
