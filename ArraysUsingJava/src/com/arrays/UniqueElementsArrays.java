605095b69c295250076c87620ac1bac676b795c6
Aniket212003 <aniketbelgaonakar21@gmail.com>
2024-08-12 19:49:41 +0530

    Status changes to Finseshed git add UniqueElementsArrays.java

--- ArraysUsingJava/src/com/arrays/UniqueElementsArrays.java
+++ ArraysUsingJava/src/com/arrays/UniqueElementsArrays.java
@@ -6,6 +6,27 @@ public class UniqueElementsArrays
 {
 	public static void uniqueElements(int arr[])
 	{
+		int max=arr[0];
+		for(int i=1; i< arr.length; i++)
+		{
+			if(arr[i]>max)
+				max = arr[i];
+		}
+		
+		int nrr[]=new int[max+1]; // 0 6
+		
+		for(int i=0; i< arr.length; i++)
+		{
+			nrr[arr[i]]++;
+		}
+		
+		for(int i=0; i< nrr.length; i++)
+		{
+			if(nrr[i]>0)
+				System.out.println(i);
+		}
+		
+		/*
 		int[] unique = new int[arr.length];
 		for(int i=0;i<arr.length;i++)
 		{
@@ -18,6 +39,7 @@ public class UniqueElementsArrays
 			}
 		}
 		System.out.println("Unique Elements : " + Arrays.toString(unique));
+       */	
 	}
 	public static void main(String[] args) 
 	{
